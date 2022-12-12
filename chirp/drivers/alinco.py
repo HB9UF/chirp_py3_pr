752f3d2d60c6c6771f4380c78e38bb2fde4a647b
Mathias Weyland <hb9frv@hb9uf.ch>
2022-12-12 14:23:33 +0100

    Mv. overrides to base cl., set NEEDS_COMPAT_SERIAL

--- chirp/drivers/alinco.py
+++ chirp/drivers/alinco.py
@@ -97,15 +97,16 @@ class AlincoStyleRadio(chirp_common.CloneModeRadio):
     """Base class for all known Alinco radios"""
     _memsize = 0
     _model = "NONE"
+    NEEDS_COMPAT_SERIAL = False
 
     def _send(self, data):
-        LOG.debug("PC->R: (%2i) %s" % (len(data), tohex(data)))
-        self.pipe.write(data.encode())
+        LOG.debug("PC->R: (%2i) %s" % (len(data), tohex(data.decode())))
+        self.pipe.write(data)
         self.pipe.read(len(data))
 
     def _read(self, length):
-        data = self.pipe.read(length).decode()
-        LOG.debug("R->PC: (%2i) %s" % (len(data), tohex(data)))
+        data = self.pipe.read(length)
+        LOG.debug("R->PC: (%2i) %s" % (len(data), tohex(data.decode())))
         return data
 
     def _download_chunk(self, addr):
@@ -156,9 +157,11 @@ class AlincoStyleRadio(chirp_common.CloneModeRadio):
 
     def _identify(self):
         for _i in range(0, 3):
-            self._send("%s\r\n" % self._model)
+            # byte-clean: compile byte instead of str
+            self._send(b"%s\r\n" % self._model)
             resp = self._read(6)
-            if resp.strip() == "OK":
+            # byte-clean: check against b"OK" instead of "OK"
+            if resp.strip() == b"OK":
                 return True
             time.sleep(1)
 
@@ -606,7 +609,6 @@ class AlincoDJG7EG(AlincoStyleRadio):
     VENDOR = "Alinco"
     MODEL = "DJ-G7EG"
     BAUD_RATE = 57600
-    NEEDS_COMPAT_SERIAL = False
 
     # Those are different from the other Alinco radios.
     STEPS = [5.0, 6.25, 8.33, 10.0, 12.5, 15.0, 20.0, 25.0, 30.0, 50.0,
@@ -661,31 +663,6 @@ class AlincoDJG7EG(AlincoStyleRadio):
         rf.memory_bounds = (0, 999)
 
         return rf
-
-    # byte-clean version of _send()
-    def _send(self, data):
-        LOG.debug("PC->R: (%2i) %s" % (len(data), tohex(data.decode())))
-        self.pipe.write(data)
-        self.pipe.read(len(data))
-
-    # byte-clean version of _read()
-    def _read(self, length):
-        data = self.pipe.read(length)
-        LOG.debug("R->PC: (%2i) %s" % (len(data), tohex(data.decode())))
-        return data
-
-    # byte-clean version of _identify()
-    def _identify(self):
-        for _i in range(0, 3):
-            # byte-clean: compile byte instead of str
-            self._send(b"%s\r\n" % self._model)
-            resp = self._read(6)
-            # byte-clean: check against b"OK" instead of "OK"
-            if resp.strip() == b"OK":
-                return True
-            time.sleep(1)
-
-        return False
 
     def _download_chunk(self, addr):
         if addr % 0x40:
